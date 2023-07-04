6ec82f7bba0a3f67c0ce8b3e3921ad80d98d49de
DevProgs <DevProgs1@gmail.com>
2023-07-04 17:39:08 -0500

    Test

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ const viewMoreBtn = document.querySelector('.blog__button');
 const viewMore = document.querySelector('.view-more');
 const viewMoreContainer = document.querySelector('.view-more--container');
 
-
 viewMoreBtn.addEventListener('click', () => {
       viewMoreContainer.classList.toggle('expanded')
       if (viewMoreContainer.classList.contains('expanded')) {
